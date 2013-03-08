6d999c7546afb3676d4a6102d6a35870e2c851db
Carl S. Yestrau Jr. <spam@featureblend.com>
2013-03-07 22:48:54 -0800

    Update Backbone.Model.FetchData.js

--- Backbone.Model.FetchData.js
+++ Backbone.Model.FetchData.js
@@ -1,5 +1,13 @@
 var FetchData = Backbone.Model.extend({
     initialize: function() {
-      
+        this.fetchData = this.options.FetchModel ? new this.options.FetchModel() || new Backbone.Model();
+        this.on('change', function() {
+            this.fetch({data: this.fetchData.toJSON()});
+        }, this);
+        this.fetch: function(method, model, options) {
+            options || (options={});
+            options = $.extend({data: this.fetchData.toJSON()}, options);
+            return Backbone.prototype.fetch.call(this, method, model, options);
+        }
     }
 });
